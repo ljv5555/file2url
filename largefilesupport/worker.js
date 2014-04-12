f776ec0086add7d7f97b9c1bab495e390e93367d
ljv5555 <jjverderber@gmail.com>
2014-04-12 14:03:51 -0500

    Create worker.js

---
+++ largefilesupport/worker.js
@@ -0,0 +1,6 @@
+onmessage=function(o)
+{
+  if(o.data.action=='reset'){currentString='';}
+  else if(o.data.action=='appendhex'){currentString+=btoa(o.data.data);}
+  else if(o.data.action=='getresult'){postMessage(currentString);}
+}
