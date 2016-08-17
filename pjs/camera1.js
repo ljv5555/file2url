91f48daf90f05c039e39a128299c4ac569a18094
ljv5555 <ljv5555@users.noreply.github.com>
2016-08-16 19:11:50 -0500

    Update camera1.js

--- pjs/camera1.js
+++ pjs/camera1.js
@@ -1,15 +1,18 @@
 var webPage = require('webpage');
 var page = webPage.create();
+var system = require('system');
+var args = system.args;
+if(!args[1] || args[1]===''){args[1]='http://ljv5555.github.io/file2url';}
 
 page.viewportSize = {
 	width: 1920,
 	height: 1080
 };
 
-page.open('http://ljv5555.github.io/scvideo', function (status) {
+page.open(args[1], function (status) {
   setTimeout(function(){
   var base64 = page.renderBase64('JPG');
-  console.log(base64);
+  console.log("<img style=\"display:none;\" src=\""+base64+"\" -data-timestamp=\""+(new Date()).getTime()+"\"/>");
   phantom.exit();
-  },8000);
+  },800);
 });
