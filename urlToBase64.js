13060891e84230f4c40e6f937b903e568a76df8b
ljv5555 <ljv5555@users.noreply.github.com>
2016-08-23 12:26:39 -0500

    Update urlToBase64.js

--- urlToBase64.js
+++ urlToBase64.js
@@ -5,15 +5,15 @@ var args = system.args;
 if(!args[1] || args[1]===''){args[1]='http://ljv5555.github.io/file2url/urlToBase64.js';}
 
 page.viewportSize = {
-	width: 1920,
-	height: 1080
+	width: 1080,
+	height: 720
 };
 
 page.open(args[1], function (status) {
   setTimeout(function(){
   var base64 = page.renderBase64('JPG');
   var ts=(new Date()).getTime();
-  console.log("<img class=\"image_\""+ts+"\ nothiddenimage hiddenimage\"  id=\"bgi_"+ts+"\" src=\"data:image/jpeg;base64,"+base64+"\"/>");
+  console.log("<img class=\"image_"+ts+"\" nothiddenimage hiddenimage\"  id=\"bgi_"+ts+"\" src=\"data:image/jpeg;base64,"+base64+"\"/>");
   phantom.exit();
   },9800);
 });
